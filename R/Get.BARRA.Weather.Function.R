662b3c05407d8492200597b389c377d4818dde6a
Fradgley, Nick (A&F, Black Mountain) <Nick.Fradgley@csiro.au>
2025-07-09 13:53:56 +1000

    fixed tests

--- R/Get.BARRA.Weather.Function.R
+++ R/Get.BARRA.Weather.Function.R
@@ -74,12 +74,6 @@ get.BARRA.weather <- function(Envs,
   }
   ncores <- min(ncores, length(Years))
 
-  if (isTRUE(ncores == 1)) { # Run in series
-    if (verbose) {
-      cat("\nRunning in series...")
-    }
-    `%dopar%` <- foreach::`%do%`
-  }
 
   cat("\nDownloading .nc files...\n")
   #DL files in series
@@ -102,6 +96,14 @@ get.BARRA.weather <- function(Envs,
       options(timeout = max(80000, getOption("timeout")))
       utils::download.file(url = addrs, destfile = tmp.files, method = "libcurl", quiet = T, mode = "wb")
     }
+    
+    if (isTRUE(ncores == 1)) { # Run in series
+      if (verbose) {
+        cat("\nRunning in series...")
+      }
+      `%dopar%` <- foreach::`%do%`
+    }
+    
     
   if (isTRUE(ncores > 1)) { # Run in parallel
     if (verbose) {
@@ -145,7 +147,7 @@ get.BARRA.weather <- function(Envs,
       env.info.yr.sub$lat.ind <- sapply(env.info.yr.sub$Lat, function(x) which.min(abs(as.numeric(dimnames(all.mons.weather)[[2]]) - as.numeric(x))))
       env.weather <- sapply(seq_len(nrow(env.info.yr.sub)), function(x) all.mons.weather[env.info.yr.sub$lon.ind[x], env.info.yr.sub$lat.ind[x], ])
       env.weather <- t(env.weather)
-      env.weather <- as.matrix(env.weather, row.names = env.info.yr.sub$Environment)[, 1:365]
+      #env.weather <- as.matrix(env.weather, row.names = env.info.yr.sub$Environment)[, 1:365]
       if (ncol(env.weather) < 365) {
         env.weather <- cbind(env.weather, matrix(NA, nrow = nrow(env.weather), ncol = 365 - ncol(env.weather)))
       }
